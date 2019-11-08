8553e73742a84420908ec9255eeb3abcb7500e54
leventerevesz <levete.revesz@gmail.com>
2019-11-08 12:06:07 +0100

    Fix some errors but still not functional

--- schedules/management/commands/evapotranspiration.py
+++ schedules/management/commands/evapotranspiration.py
@@ -5,9 +5,9 @@ class Weather:
     def __init__(self):
         self.cloud_cover = None # percentage/100
         self.humidity = None # percentage/100
-        self.pericip_intensity = None # mm/hour
+        self.pericip_intensity = None # mm/day
         self.pressure = None # kPa !!!
-        self.solar_radiation = None # MJ/m2/hour
+        self.solar_radiation = None # MJ/m2/day
         self.temperature = None # °C
         self.temperature_min = None
         self.temperature_max = None
@@ -17,6 +17,7 @@ class Weather:
         self.elevation = None # m
 
     def mean_temperature(self):
+        #return (self.temperature_max + self.temperature_min) / 2
         return self.temperature
 
     def wind_speed_2m(self):
@@ -26,36 +27,41 @@ class Weather:
     def reference_ET(self):
         "Penman-Monteith ET estimation, based on UF/IFAS AE45900"
 
+        # 1.
+        T_mean = self.mean_temperature()
+        
         # 2.
         # Mean daily solar radiation
-        R_s = self.solar_radiation # MJ/m2/hour
+        R_s = self.solar_radiation # MJ/m2/day
+
+        # 3.
+        u_2 = self.wind_speed_2m()
 
         # 4.
-        Tmean = self.mean_temperature()
-        delta = 4096 * (0.6108 * math.exp( 17.27 * Tmean / (Tmean + 273.3)) / (Tmean + 273.3)**2)
+        Delta = 4096 * (0.6108 * math.exp( 17.27 * T_mean / (T_mean + 237.3)) / (T_mean + 237.3)**2)
 
         # 6.
         C_p = 1.013e-3 # MJ/kgK
         epsilon = 0.622
         _lambda = 2.45 # MJ/kg
-        gamma = C_p * self.pressure / (epsilon * _lambda)
+        P = self.pressure # kPa 
+        gamma = C_p * P / (epsilon * _lambda)
 
         # 7.
-        u_2 = self.wind_speed_2m()
-        DT = delta / (delta + gamma * (1 + 0.34 * u_2))
+        DT = Delta / (Delta + gamma * (1 + 0.34 * u_2))
 
         # 8.
-        PT = gamma / (delta + gamma * (1 + 0.34 * u_2))
+        PT = gamma / (Delta + gamma * (1 + 0.34 * u_2))
 
         # 9.
         # modified
-        TT = (900 / 24 / (Tmean + 273.3)) * u_2
+        TT = (900 / 24 / (T_mean + 273)) * u_2
 
         # 10.
         # modified
-        T_min, T_max = Tmean, Tmean
-        e_Tmin = 0.6108 * math.exp(17.27 * T_min / (T_min + 273.3))
-        e_Tmax = 0.6108 * math.exp(17.27 * T_max / (T_max + 273.3))
+        T_min, T_max = T_mean, T_mean
+        e_Tmin = 0.6108 * math.exp(17.27 * T_min / (T_min + 237.3))
+        e_Tmax = 0.6108 * math.exp(17.27 * T_max / (T_max + 237.3))
         e_s = (e_Tmin + e_Tmax) / 2
 
         # 11.
@@ -65,19 +71,19 @@ class Weather:
         # 12.
         J = date.today().timetuple().tm_yday
         d_r = 1 + 0.033 * math.cos(2 * math.pi * J / 365)
-        _delta = 0.409 * math.sin(2 * math.pi * J / 365 - 1.39)
+        delta = 0.409 * math.sin(2 * math.pi * J / 365 - 1.39)
 
         # 13.
         phi = math.pi / 180 * self.latitude
 
         # 14.
-        omega_s = math.acos(-math.tan(phi) * math.tan(_delta))
+        omega_s = math.acos(-math.tan(phi) * math.tan(delta))
 
         # 15.
         # modified
         G_sc = 0.0820 # MJ/m2/min
         R_a = 60 / math.pi * G_sc * d_r * \
-            (omega_s * math.sin(phi) * math.sin(_delta) + math.cos(phi) * math.cos(_delta) * math.sin(omega_s))
+            (omega_s * math.sin(phi) * math.sin(delta) + math.cos(phi) * math.cos(delta) * math.sin(omega_s))
         
         # 16.
         R_so = (0.75 + 2e-5 * self.elevation) * R_a
@@ -104,7 +110,7 @@ class Weather:
 
         # Final
         ET_0 = ET_wind + ET_rad
-        breakpoint()
+        #breakpoint()
         return ET_0
 
         
