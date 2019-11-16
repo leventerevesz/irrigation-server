a4d7ab9cd04865834572eee7b1ab7f8e2a3f56f6
leventerevesz <levete.revesz@gmail.com>
2019-11-16 15:45:37 +0100

    Add Settings table in database

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Settings(models.Model):
+    weather_api_key = models.CharField(max_length=64)
+    site_latitude = models.FloatField()
+    site_longitude = models.FloatField()
+    site_elevation = models.FloatField()
+    tzcenter_longitude = models.FloatField()
+    
+    mqtt_user = models.CharField(max_length=32)
+    mqtt_password = models.CharField(max_length=32)
+    
